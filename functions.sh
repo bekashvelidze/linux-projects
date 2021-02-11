ef8606d0880516aa128266f15e01dee3a90fb7f9
bekashvelidze <78883386+bekashvelidze@users.noreply.github.com>
2021-02-11 11:38:20 +0400

    Update functions.sh
    edited comments (translated from Georgian to English)

--- functions.sh
+++ functions.sh
@@ -3,33 +3,33 @@ new_user () {
     echo "Enter name for new user:"
     echo "========================"
     read user
-    #ვხსნით სისტემის ლოგ საქაღალდეს
+    # open system logs folder
     cd /var/log
-    #ვამოწმებთ თუ არის თქვენი პროგრამის შესაბამისი ლოგ საქაღალდე
+    # check if program log folder exists
     if [ -d adduser_log/ ]; then
-    #თუ არის გადავდივართ ამ საქაღალდეში
+    # if exists, enter the folder
     cd adduser_log
     else
-    #თუ არ არის ვქმნით
+    # if doesn't exist, create
     mkdir adduser_log
     fi
-    #ვამოწმებთ არის თუ არა პროგრამის ლოგ ფაილი
+    # check if program log file exists
     if [ -f adduser_log.txt ]; then
-    #თუ არ არის ვქმნით ამ ფაილს
+    # if doesn't exist, create log file
     touch adduser_log.txt
     fi
-    #ვამოწმებთ თუ არის პროგრამის შეცდომების ლოგ ფაილი
+    # check if program error log file exists
     if [ -f adduser_error.txt ]; then
-    #თუ არ არის ვქმნით
+    # if doesn't exist, create error log file
     touch adduser_error.txt
     fi
-    #ვამოწმებთ თუ არის სისტემაში მომხმარებლის მიერ მითითებული მომხმარებელი
+    # check if entered user exists
     if id "$user" >>/var/log/adduser_log/adduser_log.txt 2>>/var/log/adduser_log/adduser_error.txt; then
     echo "============================="
-    #თუ არის გამოგვაქ შეტყობინება ამის შესახებ
+    # if user exists, print message, than exit
     echo "User already exists, exiting."
     else
-    #თუ არ არის ვქმნით
+    # if doesn't exist, create user
     adduser --gecos 1,1,1,1,1 $user
     fi
 }
@@ -37,21 +37,21 @@ del_user () {
     echo "====================================="
     echo "Enter the username you want to delete"
     read delete
-    #ვამოწმებთ თუ არის მომხმარებლის მიერ მითითებული მომხმარებელი
+    # check if entered user exists
     if id $delete >/dev/null 2>&1; then
-    #თუ არის ვშლით
+    # if exists, delete it
     deluser $delete
     else
-    #თუ არაა გამოგვაქ შეტყობინება ამის შესახებ
+    # if user doesn't exist, print message
     echo "User not found!"
     fi
-    #ვამოწმებთ თუ არის მომხმარებლის მიერ მითითებული მომხმარებლის Home საქაღალდე
+    # check if exists entered user Home folder
     if [ -d /home/$delete ]; then
-    #თუ არის ვშლით
+    # if exists, delete it
     rm -r /home/$delete
     else
-    #თუ არ არის გამოგვაქ შეტყობინება ამის შესახებ
-    echo "Directory not found!"
+    # if doesn't exist, print message
+    echo "Home directory for $delete not found!"
     fi
 }
 check_user () {
@@ -66,7 +66,7 @@ echo "User $username does not exist"
 fi
 }
 sleep .1
-#გამოგვაქვს პროგრამის ფუნქციების არჩევანი
+# List program choices
 echo "=========================================="
 echo "Create or delete users, choose what to do:"
 echo "=========================================="
@@ -80,7 +80,7 @@ echo "4. Exit"
 echo "=========================================="
 echo "Enter your choice: 1, 2, 3 or 4"
 echo "=========================================="
-#და ვასრულებთ არჩეულს
+# and execute chosen one
 read choice
 case $choice in
 1) new_user
